91c3d01a3c0a38bfdbcda0ebd8e29eec33bce620
jaq-cat <jaq.cat@protonmail.ch>
2020-12-13 12:51:11 -0300

    bruh

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include <X11/Xlib.h>
 #include <X11/extensions/XTest.h>
 #include <unistd.h>
+#include <stdio.h>
 
 #define MOVE 5
 
@@ -14,11 +15,24 @@ void set_mouse(Display *display, int x, int y) {
     XFlush(display);
 }
 
+void print_bits(char b) {
+    for (int bit=0; bit<8; bit++) {
+        printf("%i ", b & 0x01);
+        b >>= 1;
+    }
+}
+
 int main(){
     Display *display = XOpenDisplay(NULL);
-    for (int i=0; i<MOVE; i++) {
-        move_mouse(display, i, 0);
-        usleep(15000);
+    char keys_return[32];
+    while (1) {
+        printf("\e[1;1H\e[2J");
+        XQueryKeymap(display, keys_return);
+        for (int i=0; i<32; i++) {
+            print_bits(keys_return[i]);
+            printf("\n");
+        }
+        usleep(10000);
     }
     return 0;
 }
