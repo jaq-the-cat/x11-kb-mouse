6791de41cc67536b3505036f79cf71a554e02d0a
jaq-cat <jaq.cat@protonmail.ch>
2020-12-13 12:31:00 -0300

    lel

--- src/main.c
+++ src/main.c
@@ -14,25 +14,12 @@ void set_mouse(Display *display, int x, int y) {
     XFlush(display);
 }
 
-void button_make(Display *display, unsigned int button) {
-    XTestFakeButtonEvent(display, button, True, 0);
-    XFlush(display);
-}
-
-void button_break(Display *display, unsigned int button) {
-    XTestFakeButtonEvent(display, button, False, 0);
-    XFlush(display);
-
-}
-
 int main(){
     Display *display = XOpenDisplay(NULL);
     for (int i=0; i<MOVE; i++) {
         move_mouse(display, i, 0);
         usleep(15000);
     }
-    button_make(display, 1);
-    button_break(display, 1);
     return 0;
 }
 
